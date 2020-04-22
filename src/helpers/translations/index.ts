d9ccfe6f06a1a4c51d6ded078a4f9d1e029a4bef
Abhishek Prabhudesai <abhishekaprabhudesai@gmail.com>
2020-04-22 12:38:27 -0700

    Add files via upload

--- src/helpers/translations/index.ts
+++ src/helpers/translations/index.ts
@@ -4,7 +4,7 @@ import {
 } from 'fbt';
 import translations from '../../i18n/translatedFbts.json';
 
-const DEFAULT_LOCALE = 'en-US';
+const DEFAULT_LOCALE = 'en_US';
 
 export async function loadTranslations(locale: string) {
   IntlViewerContext.locale = locale || DEFAULT_LOCALE;
